1bb93433bd5b4244eca29cc4a55655fbde6ae10c
Clayton Duarte <clayton@null.net>
2021-09-02 13:36:20 -0400

    :zap: Bumps dependencies

--- src/server/mongoose/dbConnect.ts
+++ src/server/mongoose/dbConnect.ts
@@ -1,18 +1,14 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
-const uri = `${process.env.MONGO_URL}/${process.env.MONGO_DB}`;
-const options = {
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  w: "majority",
-};
+const uri = `${process.env.MONGO_URL}/${process.env.MONGO_DB}`
 
 async function dbConnect() {
   if (!mongoose.connections[0].readyState) {
-    await mongoose.connect(uri, options);
+    await mongoose.connect(uri, {
+      w: 'majority',
+    })
   }
-  return;
+  return
 }
 
-export default dbConnect;
+export default dbConnect
